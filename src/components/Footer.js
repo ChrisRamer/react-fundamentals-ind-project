1d3189946990c789aececa1a7d19ae0c76431a6f
Chris Ramer <chrisramer.97@gmail.com>
2021-10-29 11:06:09 -0700

    Add footer comp

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div id="footer">Made with ❤️ by <b>Chris Ramer (Chris is Awesome)</b> | Proudly hosted on <a href="https://github.com/ChrisRamer/react-fundamentals-ind-project">GitHub</a></div>
+	)
+}
+
+export default Footer;
