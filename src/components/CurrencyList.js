875277f0f606ef78451747bb9fb30762f07f4759
Chris Ramer <chrisramer.97@gmail.com>
2021-11-01 10:20:43 -0700

    Fix typos

--- src/components/CurrencyList.js
+++ src/components/CurrencyList.js
@@ -4,20 +4,22 @@ import PropTypes from "prop-types";
 
 function CurrencyList(props) {
 	return (
-		{props.currencytList.map((currency) =>
-			<Currency
-				whenCurrencyClicked={props.onCurrencySelection}
-				name={currency.name}
-				price={currency.price}
-				country={currency.country}
-				id={currency.id}
-				key={currency.id}/>
-		)}
+		<React.Fragment>
+			{props.currencyList.map((currency) =>
+				<Currency
+					whenCurrencyClicked={props.onCurrencySelection}
+					name={currency.name}
+					price={currency.price}
+					country={currency.country}
+					id={currency.id}
+					key={currency.id}/>
+			)}
+		</React.Fragment>
 	)
 }
 
 CurrencyList.propTypes = {
-	currencytList: PropTypes.array,
+	currencyList: PropTypes.array,
 	onCurrencySelection: PropTypes.func
 }
 
