37949835d68bf6a0166ee3ab7338ec4c5e045654
Chris Ramer <chrisramer.97@gmail.com>
2021-11-01 09:54:03 -0700

    Add CurrencyDetail

---
+++ src/components/CurrencyDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function CurrencyDetail(props) {
+	const {currency, onClickingDelete} = props;
+
+	return (
+		<React.Fragment>
+			<h1>Currency Detail</h1>
+			<h3>{currency.name}</h3>
+			<p>Country of origin: {currency.country}</p>
+			<p>Price: {currency.price}</p>
+			<button onClick={props.onClickingDelete(currency.id)}>Delete currency</button>
+			<hr />
+		</React.Fragment>
+	)
+}
+
+CurrencyDetail.propTypes = {
+	currency: PropTypes.object,
+	onClickingDelete: PropTypes.func
+}
+
+export default CurrencyDetail;
