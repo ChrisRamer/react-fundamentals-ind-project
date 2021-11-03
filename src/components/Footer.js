ff65714d63592c1d2d62a69dc2aa7ae2d08d5792
Chris Ramer <chrisramer.97@gmail.com>
2021-11-03 10:12:40 -0700

    Add role and aria-label to emoji

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
 	return (
-		<div id="footer">Made with ❤️ by <b>Chris Ramer (Chris is Awesome)</b> | Proudly hosted on <a href="https://github.com/ChrisRamer/react-fundamentals-ind-project">GitHub</a></div>
+		<div id="footer">Made with <span role="img" aria-label="Heart">❤️</span> by <b>Chris Ramer (Chris is Awesome)</b> | Proudly hosted on <a href="https://github.com/ChrisRamer/react-fundamentals-ind-project">GitHub</a></div>
 	)
 }
 
